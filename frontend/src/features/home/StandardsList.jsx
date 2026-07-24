d41ac007c0d674320a2523def8c94ca487a8de3e
Pranav Ukey <pranavukey641@gmail.com>
2026-07-24 21:07:46 +0530

    Fix standard props

--- frontend/src/features/home/StandardsList.jsx
+++ frontend/src/features/home/StandardsList.jsx
@@ -1,28 +1,5 @@
 import StandardCard from "./StandardCard";
 
-const standards = [
-  {
-    title: "EV",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Massa nec vulputate amet enim turpis elit odio fusce. Nunc cursus aliquet arcu vitae dolor ac rutrum pulvinar orci.",
-  },
-  {
-    title: "Biochar",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Massa nec vulputate amet enim turpis elit odio fusce. Nunc cursus aliquet arcu vitae dolor ac rutrum pulvinar orci.",
-  },
-  {
-    title: "Methane",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Massa nec vulputate amet enim turpis elit odio fusce. Nunc cursus aliquet arcu vitae dolor ac rutrum pulvinar orci.",
-  },
-  {
-    title: "Renewable Energy",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Massa nec vulputate amet enim turpis elit odio fusce. Nunc cursus aliquet arcu vitae dolor ac rutrum pulvinar orci.",
-  },
-];
-
 export default function StandardsList({ standards }) {
   return (
     <>
